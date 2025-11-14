04cbfa849682cfb1f0fc1f34f1f9381d6bd97f9c
Comercial Scanavini LTDA <informatica@scanavini.cl>
2025-11-13 21:01:24 -0300

    Update date.js - hora local de Chile

--- helpers/date.js
+++ helpers/date.js
@@ -1,9 +1,10 @@
 function formatMailingDate(date = new Date()) {
   return date.toLocaleString('es-CL', {
-    timeZone: 'America/Santiago',      // 👈 fuerza hora local chilena
+    timeZone: 'America/Santiago',      // fuerza hora local (Chile)
     dateStyle: 'medium',
     timeStyle: 'short'
   });
 }
+
 
 module.exports = { formatMailingDate };
